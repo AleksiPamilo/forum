356810b71c2cb88c734f1cbb9f1d864436545275
AleksiPamilo <aleksi.pamilo@live.com>
2022-10-01 22:32:14 +0300

    Update Login/SignUp form styles

--- src/components/modals/auth/LoginSignup.tsx
+++ src/components/modals/auth/LoginSignup.tsx
@@ -94,15 +94,14 @@ const Login: React.FC<LoginProps> = ({ isLogin }) => {
                         </div>
                         <Input type="email" placeholder="Email" value={email} onChange={e => setEmail(String(e.target.value))} />
                         <Input type="password" placeholder="Password" value={password} onChange={e => setPassword(String(e.target.value))} />
-
-                        <div className="flex gap-1 pt-2 justify-center">
-                            <p className="text-gray-500">Don't have an account?</p>
-                            <p className="text-blue-500 cursor-pointer hover:underline" onClick={() => { setLogin(false); clearFields(); }}>Register</p>
-                        </div>
-                        <div className="flex pb-2 justify-center">
+                        <div className="flex pb-2">
                             <p className="text-blue-500 cursor-pointer hover:underline" onClick={() => { handlePasswordModal(); clearFields(); }}>Forgot Password?</p>
                         </div>
                         <Button styles="w-full px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white" onClick={handleLogin}>Login</Button>
+                        <div className="flex gap-1 py-2 justify-center">
+                            <p className="text-gray-500">Don't have an account?</p>
+                            <p className="text-blue-500 cursor-pointer hover:underline" onClick={() => { setLogin(false); clearFields(); }}>Register</p>
+                        </div>
                     </div>
 
                     : <div className="flex flex-col gap-2">
@@ -116,15 +115,17 @@ const Login: React.FC<LoginProps> = ({ isLogin }) => {
                         <Input type="email" placeholder="Email" value={email} onChange={e => setEmail(String(e.target.value))} />
                         <Input type="password" autoComplete="new-password" placeholder="Password" value={password} onChange={e => setPassword(String(e.target.value))} />
                         <Input type="password" autoComplete="new-password" placeholder="Confirm Password" value={confirmPassword} onChange={e => setConfirmPassword(String(e.target.value))} />
-                        <span>Profile Picture (optional)</span>
-                        <Dropzone onChange={(files) => setPhotoFile(files[0])} acceptedFiles={{
-                            "image/*": [".jpg", ".jpeg", ".png"]
-                        }} />
+                        <div className="pb-2">
+                            <span>Profile Picture (optional)</span>
+                            <Dropzone onChange={(files) => setPhotoFile(files[0])} acceptedFiles={{
+                                "image/*": [".jpg", ".jpeg", ".png"]
+                            }} />
+                        </div>
+                        <Button styles="w-full px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white" onClick={handleRegister}>Register</Button>
                         <div className="flex gap-1 py-2 justify-center">
                             <p className="text-gray-500">Already have an account?</p>
                             <p className="text-blue-500 cursor-pointer hover:underline" onClick={() => { setLogin(true); clearFields(); }}>Login</p>
                         </div>
-                        <Button styles="w-full px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white" onClick={handleRegister}>Register</Button>
                     </div>
             }
             <div className="justify-center mt-4 w-full px-4 py-2 rounded-md bg-red-500 text-white" style={{ display: !!errorMessage ? "flex" : "none" }}>
