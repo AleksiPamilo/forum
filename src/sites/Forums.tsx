a3773e21bbb9384594d75547ce453f0ce4fae3d3
AleksiPamilo <aleksi.pamilo@live.com>
2022-10-28 16:05:14 +0300

    Update styles

--- src/sites/Forums.tsx
+++ src/sites/Forums.tsx
@@ -10,7 +10,7 @@ const Forums: React.FC = () => {
             <div className="flex flex-col gap-8">
                 {categories.map((category) => (
                     <div key={category.id} className="flex flex-col divide-y divide-blue-800 border border-blue-600 shadow-[0_0_10px_4px_rgba(33,54,163)] rounded-[5px]">
-                        <h2 className="text-xl font-bold p-2 bg-blue-800 text-black rounded-t-sm">{category.name}</h2>
+                        <h2 className="text-xl font-bold p-2 bg-blue-900 text-black rounded-t-sm">{category.name}</h2>
                         {
                             forums.map((forum) => (
                                 forum.categoryId === category.id ? <ForumCard forum={forum} /> : null
